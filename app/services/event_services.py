3c1fb21495dbb486b40b06d85d293167434fac3c
Ankit Shukla <ankit.shukla.sp@dlh.de>
2026-07-11 15:08:22 +0200

    Publishing events

--- app/services/event_services.py
+++ app/services/event_services.py
@@ -9,5 +9,8 @@ def publish_events(db):
         if statement is not None:
             MessageEvents.status = "processed"
             print("Event Published")
+            db.commit(MessageEvents)
+            db.refresh(MessageEvents)
+            return MessageEvents
         else:
             print("No pending event found")
